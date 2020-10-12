c0f75ea8b34f78645bb4a1af856f45d01b4c6ddd
Shisin20 <66086260+Shisin20@users.noreply.github.com>
2020-10-12 23:37:04 +0530

    First Commit

---
+++ TechChallenge.App/ApplicationHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Navigation;
+
+namespace TechChallenge.App
+{
+    //NavigationContext static helper class 
+    public static class ApplicationHelper
+    {
+        private static NavigationService _navigationService;
+
+        public static NavigationService NavigationService
+        {
+            set
+            {
+                _navigationService = value;
+            }
+            get
+            {
+                return _navigationService;
+            }
+
+        }
+    }
+}
---
+++ TechChallenge.App/ViewModelLocator.cs
@@ -0,0 +1,29 @@
+﻿namespace TechChallenge.App
+{
+    public class ViewModelLocator
+    {
+        //private static ITaskDataService taskDataService = new TaskDataService(new TaskRepository());
+
+        //private static ILoginDataService loginDataService = new LoginDataService();
+
+        //private static TaskOverviewViewModel overviewViewModel = new TaskOverviewViewModel(taskDataService);
+
+        //private static LoginViewModel loginViewModel = new LoginViewModel(loginDataService);
+
+        //public static TaskOverviewViewModel TaskOverviewViewModel
+        //{
+        //    get
+        //    {
+        //        return overviewViewModel;
+        //    }
+        //}
+
+        //public static LoginViewModel LoginViewModel
+        //{
+        //    get
+        //    {
+        //        return loginViewModel;
+        //    }
+        //}
+    }
+}
